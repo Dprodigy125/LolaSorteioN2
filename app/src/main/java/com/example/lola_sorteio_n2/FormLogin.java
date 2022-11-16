2b265beb4137b53659e680e11a7a3ee9fd44b43f
Roberto Aguiar <r.aguiar@adinstruments.com>
2022-11-16 18:05:18 -0300

    Atualizacao Paula - 16/11 telas

---
+++ app/src/main/java/com/example/lola_sorteio_n2/FormLogin.java
@@ -0,0 +1,13 @@
+package com.example.lola_sorteio_n2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
