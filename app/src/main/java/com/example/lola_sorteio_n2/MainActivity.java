be7cdf791918341dc6d358802db638b3096556e1
Roberto Aguiar <r.aguiar@adinstruments.com>
2022-11-22 18:59:20 -0300

    Atualizacao - Botão inicio + Layout arrumado

--- app/src/main/java/com/example/lola_sorteio_n2/MainActivity.java
+++ app/src/main/java/com/example/lola_sorteio_n2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lola_sorteio_n2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void telaLogin(View view)
+    {
+        Intent secondActivity = new Intent(this, FormLogin.class);
+        startActivity(secondActivity);
     }
 }
