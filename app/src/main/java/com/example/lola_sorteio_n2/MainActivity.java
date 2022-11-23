ad209b482b8f2a33cd214f362c39880ef130bcb6
Dprodigy125 <souzadiogo357@gmail.com>
2022-11-22 21:43:41 -0300

    Tentativa de tela logada...

--- app/src/main/java/com/example/lola_sorteio_n2/MainActivity.java
+++ app/src/main/java/com/example/lola_sorteio_n2/MainActivity.java
@@ -11,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Tela_login(view view)
+    {
+
     }
 
     public void telaLogin(View view)
