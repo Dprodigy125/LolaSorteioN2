4ac97ac2d606c4eb2382d953ff388e8439e71830
Roberto Aguiar <r.aguiar@adinstruments.com>
2022-11-22 22:36:17 -0300

    Tela usuário criada - titulos das views - leve arrumada

--- app/src/main/java/com/example/lola_sorteio_n2/FormLogin.java
+++ app/src/main/java/com/example/lola_sorteio_n2/FormLogin.java
@@ -1,7 +1,9 @@
 package com.example.lola_sorteio_n2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FormLogin extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class FormLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+    }
+
+    public void telaLogado(View view)
+    {
+        Intent secondActivity = new Intent(this, Tela_Logada.class);
+        startActivity(secondActivity);
     }
 }
