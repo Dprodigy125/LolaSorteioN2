138a25463fa5ae2282ca1ee00032db66a110a0f3
Roberto Aguiar <r.aguiar@adinstruments.com>
2022-11-23 01:26:04 -0300

    Telas prontas e botões funcionando. Cadastro não funcionando.

--- app/src/main/java/com/example/lola_sorteio_n2/TelaCadastro.java
+++ app/src/main/java/com/example/lola_sorteio_n2/TelaCadastro.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class TelaCadastro extends AppCompatActivity {
 
@@ -11,5 +12,10 @@ public class TelaCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+    public void telaLogado(View view)
+    {
+        Intent secondActivity = new Intent(this, Tela_Logada.class);
+        startActivity(secondActivity);
     }
 }
